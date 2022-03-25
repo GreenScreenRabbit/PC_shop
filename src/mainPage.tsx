dcd9d4f145f96bae5c71e26ddcf6e9f8149c595e
GreenScreenRabbit <playsosnolka1@gmail.com>
2022-03-25 05:56:04 +0200

    start

---
+++ src/mainPage.tsx
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import "./mainPage.css";
+
+const MainPage = () => {
+    return (
+        <>
+            <div className="mainPage">4</div>
+        </>
+    );
+};
+
+export default connect(null, null)(MainPage);
